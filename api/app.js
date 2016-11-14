0290c41b9d8158909c5edc924e26a13c708916f9
Mitchell Sablosky <msablosk@gmail.com>
2016-11-14 15:02:14 +0100

    api is done

--- api/app.js
+++ api/app.js
@@ -3,32 +3,34 @@ var fetch = require('node-fetch');
 var key = require('./key');
 var app = express();
 
-
-
-var url = "https://www.quandl.com/api/v3/datasets/GOOG/NYSE_BND.json?rows=1?api_key=" + key.key
-
-
-
-
 //Get the last end of day price for VTI
+var urlVTI = "https://www.quandl.com/api/v3/datasets/GOOG/NYSE_VTI.json?rows=1?api_key=" + key.key
 //Get the last end of day price for VXUS
+var urlVXUS = "https://www.quandl.com/api/v3/datasets/GOOG/NASDAQ_VXUS.json?rows=1?api_key=" + key.key
 //Get the last end of day price for BND
-var url = "https://www.quandl.com/api/v3/datasets/GOOG/NYSE_BND.json?rows=1?api_key=" + key.key
+var urlBND = "https://www.quandl.com/api/v3/datasets/GOOG/NYSE_BND.json?rows=1?api_key=" + key.key
 
+function getQuote(url) {
+    // RETURN the promise
+    return fetch(url).then(function(response){
+        return response.json(); // process it inside the `then`
+    });
+}
 
 app.get('/', function (req, res) {
 		
-	var bnd;
+	var vti, vxus, bnd;
 	
-	fetch(url)
-	.then(function(res) {
-    	return res.json();
-	}).then(function(json) {
-    	bnd = json.dataset.data[0][4];
-    	res.send('Hello World! ' + bnd)
+	getQuote(urlVTI).then(function(response){
+    	vti = response.dataset.data[0][4];
+    	getQuote(urlVXUS).then(function(response){
+    		vxus = response.dataset.data[0][4];
+    		getQuote(urlBND).then(function(response){
+    			bnd = response.dataset.data[0][4];
+    			res.json({"vti": vti, "vxus": vxus, "bnd": bnd})
+    		})
+    	})
 	})
-
-	
 })
 
 
